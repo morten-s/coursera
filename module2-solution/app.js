07830ff5a01bc951c61ca33e75272efff2ee3b4b
morten-s <ms@meetpeople.dk>
2016-10-07 13:51:07 +0200

    module2

---
+++ module2-solution/app.js
@@ -0,0 +1,30 @@
+/*
+ * MVS
+ */
+(function (){
+ 'use strict';
+
+ angular.module('LunchCheck', [])
+         .controller('LunchCheckController', LunchCheckController);
+
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope){
+        $scope.LunchCheck = function(){
+            //check if dishes is not defined as in empty
+            if(!$scope.dishes){
+                $scope.message = "Please enter data";
+            }else{
+                //test if there are more than 3 items
+                if($scope.dishes.split(",").length <= 3 ){
+                    $scope.message = "Enjoy!";
+                }else{
+                    $scope.message = "Too much";
+                
+                }
+            }
+        };
+    }
+
+ })();
